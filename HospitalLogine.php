b08f7f72dc594f34b067bb68156e5b306996554a
Rajendra pathak <rajendrapathak1988@gmail.com>
2020-11-21 18:26:11 +0530

    Update HospitalLogine.php

--- HospitalLogine.php
+++ HospitalLogine.php
@@ -9,7 +9,7 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Login</title>
-    <link rel="stylesheet" href="php-pages/css/s1.css">
+    <link rel="stylesheet" href="css/s1.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
 </head>
